a2e5def6a378633238642367a80184a1d5a3ee66
Arkaaito <arkaaito@scion-interactive.com>
2016-04-02 03:47:14 -0700

    Writing questionnaire responses back to the server.

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from fhirclient import client
@@ -19,6 +19,7 @@ def respond(request):
     }
     smart = client.FHIRClient(settings=settings)
     patientId = request.COOKIES.get('userId')
+
     #childRecord = patient.Patient.read('18907426', smart.server)
     childRecord = patient.Patient.read(patientId, smart.server)
     #smart.human_name(childRecord.name[0])
@@ -28,10 +29,31 @@ def respond(request):
     if age < 13:
         form = questionnaire.Questionnaire.read("18791830", smart.server)
 
+    jsonResponse = []
+    if request.method == 'POST':
+        jsonResponse = {"group": {"linkId": "root", "question": []}, "resourceType": "QuestionnaireResponse",
+                        "questionnaire": {"reference": "Questionnaire/"+form.id}, "status": "completed",
+                        "subject": {"reference": "Patient/"+childRecord.id},
+                        "author": {"reference": "Patient/"+childRecord.id}}
+        for question in form.group.question:
+            questionJson = {"linkId": question.linkId, "answer": [{"valueInteger": request.POST[question.linkId]}]}
+            jsonResponse["group"]["question"].append(questionJson)
+        smart.server.post_json('QuestionnaireResponse', jsonResponse)
+        #response = questionnaireresponse.QuestionnaireResponse()
+        #response.group = questionnaireresponse.QuestionnaireResponseGroup()
+        #for questionId in request.POST.keys:
+        #    question = questionnaireresponse.QuestionnaireResponseGroupQuestion()
+        #    question.linkId = questionId
+        #    question.answer = questionnaireresponse.QuestionnaireResponseGroupQuestionAnswer()
+        #    question.answer.valueString = request.POST[questionId]
+        #    response.group.question.add(question)
+
     context = RequestContext(request)
     context['patientName'] = smart.human_name(childRecord.name[0])
     context['questionnaire'] = form.group.question
+    context['json'] = jsonResponse
 
+    #return JsonResponse(jsonResponse)
     return render_to_response('questionnaire.html',
                               context_instance=context)
 
