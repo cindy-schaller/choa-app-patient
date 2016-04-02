49e3700914b5d5a57ecde6caef429dc10387ca24
Arkaaito <arkaaito@scion-interactive.com>
2016-04-02 03:47:52 -0700

    Removing some no-longer-needed comments and alerts.

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -20,9 +20,7 @@ def respond(request):
     smart = client.FHIRClient(settings=settings)
     patientId = request.COOKIES.get('userId')
 
-    #childRecord = patient.Patient.read('18907426', smart.server)
     childRecord = patient.Patient.read(patientId, smart.server)
-    #smart.human_name(childRecord.name[0])
     age = (datetime.now().date() - childRecord.birthDate.date).days / 365.24
 
     form = questionnaire.Questionnaire.read("18791835", smart.server)
@@ -39,14 +37,6 @@ def respond(request):
             questionJson = {"linkId": question.linkId, "answer": [{"valueInteger": request.POST[question.linkId]}]}
             jsonResponse["group"]["question"].append(questionJson)
         smart.server.post_json('QuestionnaireResponse', jsonResponse)
-        #response = questionnaireresponse.QuestionnaireResponse()
-        #response.group = questionnaireresponse.QuestionnaireResponseGroup()
-        #for questionId in request.POST.keys:
-        #    question = questionnaireresponse.QuestionnaireResponseGroupQuestion()
-        #    question.linkId = questionId
-        #    question.answer = questionnaireresponse.QuestionnaireResponseGroupQuestionAnswer()
-        #    question.answer.valueString = request.POST[questionId]
-        #    response.group.question.add(question)
 
     context = RequestContext(request)
     context['patientName'] = smart.human_name(childRecord.name[0])
