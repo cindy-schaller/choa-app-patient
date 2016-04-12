d080109b177da5ee77a25df0ac9469fc0243f3d4
Arkaaito <arkaaito@scion-interactive.com>
2016-04-12 00:32:44 -0700

    Redirecting the patient to the login location (well, the patient select screen) if they haven't logged in yet.

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render,render_to_response,redirect
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
@@ -28,36 +28,39 @@ def index(request):
 def respond(request):
     smart = getFhirClient()
     patientId = request.COOKIES.get('userId')
+    if patientId:
+        try:
+            childRecord = patient.Patient.read(patientId, smart.server)
+            age = (datetime.now().date() - childRecord.birthDate.date).days / 365.24
 
-    try:
-        childRecord = patient.Patient.read(patientId, smart.server)
-        age = (datetime.now().date() - childRecord.birthDate.date).days / 365.24
+            form = questionnaire.Questionnaire.read("18791835", smart.server)
+            if age < 13:
+                    form = questionnaire.Questionnaire.read("18791830", smart.server)
 
-        form = questionnaire.Questionnaire.read("18791835", smart.server)
-        if age < 13:
-                form = questionnaire.Questionnaire.read("18791830", smart.server)
+            jsonResponse = []
+            if request.method == 'POST':
+                jsonResponse = {"group": {"linkId": "root", "question": []}, "resourceType": "QuestionnaireResponse",
+                                "questionnaire": {"reference": "Questionnaire/"+form.id}, "status": "completed",
+                                "subject": {"reference": "Patient/"+childRecord.id},
+                                "author": {"reference": "Patient/"+childRecord.id}}
+                for question in form.group.question:
+                    questionJson = {"linkId": question.linkId, "answer": [{"valueInteger": request.POST[question.linkId]}]}
+                    jsonResponse["group"]["question"].append(questionJson)
+                smart.server.post_json('QuestionnaireResponse', jsonResponse)
 
-        jsonResponse = []
-        if request.method == 'POST':
-            jsonResponse = {"group": {"linkId": "root", "question": []}, "resourceType": "QuestionnaireResponse",
-                            "questionnaire": {"reference": "Questionnaire/"+form.id}, "status": "completed",
-                            "subject": {"reference": "Patient/"+childRecord.id},
-                            "author": {"reference": "Patient/"+childRecord.id}}
-            for question in form.group.question:
-                questionJson = {"linkId": question.linkId, "answer": [{"valueInteger": request.POST[question.linkId]}]}
-                jsonResponse["group"]["question"].append(questionJson)
-            smart.server.post_json('QuestionnaireResponse', jsonResponse)
+            context = RequestContext(request)
+            context['patientName'] = smart.human_name(childRecord.name[0])
+            context['questionnaire'] = form.group.question
+            context['json'] = jsonResponse
 
-        context = RequestContext(request)
-        context['patientName'] = smart.human_name(childRecord.name[0])
-        context['questionnaire'] = form.group.question
-        context['json'] = jsonResponse
+            #return JsonResponse(jsonResponse)
+            return render_to_response('questionnaire.html',
+                                      context_instance=context)
+        except FHIRNotFoundException:
+            return HttpResponse("The user you've selected appears to be invalid.  Please return to <a href='/questionnaire/'>the index</a> and select a different user.")
+    else:
+        return redirect("/questionnaire/")
 
-        #return JsonResponse(jsonResponse)
-        return render_to_response('questionnaire.html',
-                                  context_instance=context)
-    except FHIRNotFoundException:
-        return HttpResponse("The user you've selected appears to be invalid.  Please return to <a href='/questionnaire/'>the index</a> and select a different user.")
 
 def messages(request):
     smart = getFhirClient()
