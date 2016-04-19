84e8127c36559d7ae2861e353afb0961c400b7fd
Arkaaito <arkaaito@scion-interactive.com>
2016-04-18 17:38:07 -0700

    Adding fgh/templates directory in settings to allow templates that aren't under the questionnaire app.

--- fgh/settings.py
+++ fgh/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'fgh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'fgh/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
