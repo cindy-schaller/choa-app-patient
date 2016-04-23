cf59e5a88653bd22d6dec4fb165c5a80ebb34ae1
Arkaaito <arkaaito@scion-interactive.com>
2016-04-22 19:35:22 -0700

    Switching to point to SMART on FHIR server.

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render,render_to_response,redirect
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext
@@ -16,7 +18,8 @@ from fhirclient.server import FHIRNotFoundException
 def getFhirConnectionInfo():
     return {
         'app_id': 'omscs-hit-cdc',
-        'api_base': 'http://52.72.172.54:8080/fhir/baseDstu2'
+        #'api_base': 'http://52.72.172.54:8080/fhir/baseDstu2'
+        'api_base': 'https://fhir-open-api-dstu2.smarthealthit.org/'
     }
 
 
@@ -34,21 +37,66 @@ def resolveFhirReference(fhir_reference, server):
     else:
         raise FHIRNotFoundException("Unsupported class "+klassname+" in reference "+fhir_reference.reference)
 
+
+def setupFhirServer(server):
+    patients = {'18791941': 'patient1.json', '18791962': 'patient2.json', '18791983': 'patient3.json', '18792004': 'patient4.json'}
+    for patient in patients.keys():
+        with open('json_data/'+patients[patient], 'r') as h:
+            pjson = json.load(h)
+            #server.post_json('Patient', pjson)
+            #server.put_json('Patient/'+patient, pjson)
+    questionnaires = {'18791835': 'questionnaire-adolescent.json', '18791830': 'questionnaire-child.json'}
+    for questionnaire in questionnaires:
+        with open('json_data/'+questionnaires[questionnaire], 'r') as h:
+            qjson = json.load(h)
+            server.post_json('Questionnaire', qjson)
+            #server.put_json('Questionnaire/'+questionnaire, qjson)
+    for message in ['message1.json']:
+       with open('json_data/'+message, 'r') as h:
+            mjson = json.load(h)
+            server.post_json('Communication', mjson)
+
+
+def getResources(server):
+    print "Loading"
+    search = patient.Patient.where(struct={"name": "Kent"})
+    patients = search.perform(server)
+    print patients.entry[0].resource.id
+    search = patient.Patient.where(struct={"name": "Prince"})
+    patients = search.perform(server)
+    print patients.entry[0].resource.id
+    search = patient.Patient.where(struct={"name": "Prior"})
+    patients = search.perform(server)
+    print patients.entry[0].resource.id
+    search = patient.Patient.where(struct={"name": "Eaton"})
+    patients = search.perform(server)
+    print patients.entry[0].resource.id
+
+    search = questionnaire.Questionnaire.where(struct={"title": "Health Habits Tracker (adolescent)"})
+    questionnaires = search.perform(server)
+    print questionnaires.entry[0].resource.id
+
+    search = questionnaire.Questionnaire.where(struct={"title": "Healthy Habits Questionnaire"})
+    questionnaires = search.perform(server)
+    print questionnaires.entry[1].resource.id
+
+
 def index(request):
     return render_to_response('index.html',
                               context_instance=RequestContext(request))
 
 def respond(request):
     smart = getFhirClient()
+    getResources(smart.server)
     patientId = request.COOKIES.get('userId')
     if patientId:
         try:
             childRecord = patient.Patient.read(patientId, smart.server)
             age = (datetime.now().date() - childRecord.birthDate.date).days / 365.24
 
-            form = questionnaire.Questionnaire.read("18791835", smart.server)
+            form = questionnaire.Questionnaire.read("571adc040cf20e9addb27240", smart.server)
             if age < 13:
-                    form = questionnaire.Questionnaire.read("18791830", smart.server)
+                    form = questionnaire.Questionnaire.read("571adc030cf20e9addb2723f", smart.server)
 
             jsonResponse = []
             if request.method == 'POST':
