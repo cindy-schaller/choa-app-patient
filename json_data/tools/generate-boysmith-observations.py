a7295fa2100fc228612fde90aad1b7841d033b33
Dana Sheahen <danasheahen@gatech.edu>
2016-06-21 22:50:26 -0500

    created observation data for clark kent;  wt data crashes the system, bmi is wonderful, ht is hit or miss

--- json_data/tools/generate-boysmith-observations.py
+++ json_data/tools/generate-boysmith-observations.py
@@ -1,39 +1,83 @@
 from __future__ import print_function
 import json
+import os
 from collections import OrderedDict
-from fhirclient.models import fhirdate, observation
+from fhirclient.models import fhirdate, observation, patient
 from observation_commons import *
-import datetime
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
+patient_id = "11034584"
+patient_id_ref = "Patient/"+patient_id
+patient_name = "Clark Kent"
+patient_dob = datetime(2011,3,30)
+
+# Observation/11037412/_history/1
 # data for generation (provided by Aly)
 # Boy Smith 2-5 yr
 # Visit    Age(mos)   Wt(lbs)  Ht(in)  BMI(kg/m2)
 observation_list = [
-    ["MD",  24, 30, 34.5, 17.7],
-    ["WIC", 24, 30, 34.5, 17.7],
-    ["WIC", 30, 34, 36.25, 18],
-    ["WIC", 36, 36, 38, 17.5],
-    ["MD",  39, 38, 38.25, 18],
-    ["WIC", 48, 44, 40, 19],
-    ["MD",  51, 44, 40, 19],
-    ["WIC", 52, 44, 40, 19],
-    ["WIC", 54, 46, 40.25, 19.9],
+    ["MD",  24, 30., 34.5, 17.7],
+    ["WIC", 24, 30., 34.5, 17.7],
+    ["WIC", 30, 34., 36.25, 18],
+    ["WIC", 36, 36., 38., 17.5],
+    ["MD",  39, 38., 38.25, 18],
+    ["WIC", 48, 44., 40., 19],
+    ["MD",  51, 44., 40., 19],
+    ["WIC", 52, 44., 40., 19],
+    ["WIC", 54, 46., 40.25, 19.9],
     ["WIC", 57, 46.5, 40.5, 19.9],
-    ["WIC", 60, 48, 42, 19.1],
+    ["WIC", 60, 48., 42., 19.1],
 ]
+
+def lbs_to_kg(lbs):
+    return round(lbs*0.453592,1)
+def in_to_cm(ins):
+    return round(ins*2.54,1)
 
 if __name__ == '__main__':
     # set up observation structure from model
     for i in range(len(observation_list)):
+        performer, age_mos, wt_lbs, ht_in, bmi = observation_list[i]
         o = observation.Observation()
         o.status = 'final'
-        o.code = code_root
-        # add subject, encounter, effectiveDateTime, performer ?
-        performer, age_mos, wt_lbs, ht_in, bmi = observation_list[i]
-        o.component=[
-            {"code":coding_weight, "valueQuantity": {"value":wt_lbs,"unit":"lb_av"}},
-            {"code":coding_height, "valuequantity": {"value":ht_in,"unit":"in_i"}},
-            {"code":coding_bmi,"valueQuantity": {"value":bmi,"unit":"kg/m2"}},
-            ]
-        with open('ob-clark-'+str(i)+'.json','w') as f:
+        o.subject = {"reference":patient_id_ref, "display":patient_name}
+        o.encounter = [{"display":performer}]
+        o.performer = [{"display":performer}]
+        date_seen = patient_dob + relativedelta(months=age_mos)
+        o.effectiveDateTime = date_seen.isoformat()
+
+        o.code = coding_weight
+        o.valueQuantity = {"value":lbs_to_kg(wt_lbs),"unit":"kg"}
+        with open(os.path.join('ob-clark','ob-clark-wt-'+str(i)+'.json'),'w') as f:
             print(json.dumps(OrderedDict(o.as_json()), indent=4, separators=(',', ': ')), file=f)
+
+        o.code = coding_height
+        o.valueQuantity = {"value":in_to_cm(ht_in),"unit":"cm"}
+        with open(os.path.join('ob-clark', 'ob-clark-ht-' + str(i) + '.json'), 'w') as f:
+            print(json.dumps(OrderedDict(o.as_json()), indent=4, separators=(',', ': ')), file=f)
+
+        o.code = coding_bmi
+        o.valueQuantity = {"value": bmi, "unit": "kg/m2"}
+        with open(os.path.join('ob-clark', 'ob-clark-bmi-' + str(i) + '.json'), 'w') as f:
+            print(json.dumps(OrderedDict(o.as_json()), indent=4, separators=(',', ': ')), file=f)
+
+
+        # o = observation.Observation()
+        # o.status = 'final'
+        # o.code = code_root
+        #
+        # # add subject, encounter, effectiveDateTime, performer ?
+        # o.subject = {"reference":patient_id_ref, "display":patient_name}
+        # o.encounter = [{"display":performer}]
+        # o.performer = [{"display":performer}]
+        # date_seen = patient_dob + relativedelta(months=age_mos)
+        # o.effectiveDateTime = date_seen.isoformat()
+        #
+        # o.component=[
+        #     {"code":coding_weight, "valueQuantity": {"value":wt_lbs,"unit":"lb_av"}},
+        #     {"code":coding_height, "valueQuantity": {"value":ht_in,"unit":"in_i"}},
+        #     {"code":coding_bmi,"valueQuantity": {"value":bmi,"unit":"kg/m2"}},
+        #     ]
+        # with open(os.path.join('ob-clark','ob-clark-'+str(i)+'.json'),'w') as f:
+        #     print(json.dumps(OrderedDict(o.as_json()), indent=4, separators=(',', ': ')), file=f)
--- json_data/tools/observation_commons.py
+++ json_data/tools/observation_commons.py
@@ -2,22 +2,22 @@ code_root = {"coding": [{
         "system": "http://loinc.org",
         "code": "34565-2",
         "display": "Vital signs, weight and height panel",
-      }]}
+}]}
 
-coding_weight = {
+coding_weight = {"coding": [{
             "system": "http://loinc.org",
             "code": "3141-9",
-            "display": "Body weight Measured"
-          }
+            "display": "Weight"
+}]}
 
-coding_height = {
+coding_height = {"coding": [{
             "system": "http://loinc.org",
             "code": " 8302-2",
-            "display": "Body height"
-          }
+            "display": "Height"
+}]}
 
-coding_bmi = {
+coding_bmi = {"coding": [{
             "system": "http://loinc.org",
             "code": "39156-5",
-            "display": "Body mass index (BMI) [Ratio]"
-          }
+            "display": "BMI"
+}]}
