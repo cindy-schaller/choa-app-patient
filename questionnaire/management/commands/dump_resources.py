29fff773cc4d37b1b879e831da3eb0b26cb70d54
Dana Sheahen <danasheahen@gatech.edu>
2016-07-19 08:55:12 -0500

    add additional lookup resources; refactor to check if found

--- questionnaire/management/commands/dump_resources.py
+++ questionnaire/management/commands/dump_resources.py
@@ -1,10 +1,6 @@
 from django.core.management.base import BaseCommand
-
-from fhirclient.models import patient, questionnaire, questionnaireresponse, communication, practitioner
-
+from fhirclient.models import patient, questionnaire, questionnaireresponse, organization
 from questionnaire import utils
-
-patients = ['patient1.json', 'patient2.json', 'patient3.json', 'patient4.json', 'patient5.json']
 
 
 class Command(BaseCommand):
@@ -18,30 +14,41 @@ class Command(BaseCommand):
         client = utils.getFhirClient(options['server-id'][0])
         server = client.server
 
-        search = patient.Patient.where(struct={"name": "Kent"})
-        patients = search.perform(server)
-        for entry in patients.entry:
-            print "Clark Kent: " + entry.resource.id
-        search = patient.Patient.where(struct={"name": "Prince"})
-        patients = search.perform(server)
-        for entry in patients.entry:
-            print "Diana Prince: " + entry.resource.id
-        search = patient.Patient.where(struct={"name": "Prior"})
-        patients = search.perform(server)
-        for entry in patients.entry:
-            print "Beatrice Prior: " + entry.resource.id
-        search = patient.Patient.where(struct={"name": "Eaton"})
-        patients = search.perform(server)
-        for entry in patients.entry:
-            print "Tobias Eaton: " + entry.resource.id
+        # resource type, /search criteria, print name
+        data = [
+            ("Patient", {"family": "Kent", "given": "Clark"}, "Clark Kent"),
+            ("Patient", {"family": "Kent", "given": "Kara"}, "Kara Kent"),
+            ("Patient", {"family": "Prince", "given": "Diana"}, "Diana Prince"),
+            ("Patient", {"family": "Prior", "given": "Beatrice"}, "Beatrice Prior"),
+            ("Patient", {"family": "Eaton", "given": "Tobias"}, "Tobias Eaton"),
+            ("Questionnaire", {"title": "Healthy Habits Tracker (adolescent)"}, "Adolescent questionnaire"),
+            ("Questionnaire", {"title": "Healthy Habits Questionnaire"}, "Child questionnaire"),
+            ("Questionnaire", {"title": "WIC Child Nutrition Questionnaire"}, "WIC questionnaire"),
+            ("Questionnaire", {"title": "Healthy Habits Goal Questionnaire"}, "Goals questionnaire"),
+            ("Questionnaire", {"title": "Healthy Habits Goal Status"}, "Goal Status questionnaire"),
+            ("Questionnaire", {"title": "Food Insecurity Questionnaire"}, "Food Insecurity questionnaire"),
+            ("Organization", {"name": "WIC"}, "WIC organization"),
+            ("Organization", {"name": "MD"}, "MD organization"),
+            ("Organization", {"name": "Patient Care Coordinator"}, "Patient Care Coordinator organization"),
+        ]
 
-        print ""
-        search = questionnaire.Questionnaire.where(struct={"title": "Health Habits Tracker (adolescent)"})
-        questionnaires = search.perform(server)
-        for entry in questionnaires.entry:
-            print "Adolescent questionnaire: " + entry.resource.id
+        for item in data:
+            valid_entry = True
+            r_type, criteria, output = item
+            if r_type == "Patient":
+                search = patient.Patient.where(struct=criteria)
+            elif r_type == "Questionnaire":
+                search = questionnaire.Questionnaire.where(struct=criteria)
+            elif r_type == "Organization":
+                search = organization.Organization.where(struct=criteria)
+            else:
+                valid_entry = False
+                print "Invalid entry in lookup list"
 
-        search = questionnaire.Questionnaire.where(struct={"title": "Healthy Habits Questionnaire"})
-        questionnaires = search.perform(server)
-        for entry in questionnaires.entry:
-            print "Child questionnaire: " + entry.resource.id
+            if valid_entry:
+                resources = search.perform(server)
+                if resources.total > 0:
+                    for entry in resources.entry:
+                        print output + ": " + entry.resource.id
+                else: print output + ": not found"
+
